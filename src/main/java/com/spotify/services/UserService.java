e23688f9d8d298fd3dca38df78e8d635aa19770e
Art <paganotiarthur@gmail.com>
2025-10-15 20:47:48 -0300

    :sparkles: feat: implements user registration service with email validation

---
+++ src/main/java/com/spotify/services/UserService.java
@@ -0,0 +1,34 @@
+package com.spotify.services;
+
+import com.spotify.business.dto.UserRegisterDTO;
+import com.spotify.business.ResponseDTO;
+import com.spotify.entities.User;
+import com.spotify.repositories.UserRepository;
+import com.spotify.exceptions.EmailAlreadyExistsException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = new BCryptPasswordEncoder();
+    }
+
+    @Transactional
+    public ResponseDTO<String> register(UserRegisterDTO dto) {
+        if (userRepository.findByEmail(dto.getEmail()).isPresent()) {
+            throw new EmailAlreadyExistsException("Email já cadastrado");
+        }
+        User user = new User();
+        user.setName(dto.getName());
+        user.setEmail(dto.getEmail());
+        user.setPassword(passwordEncoder.encode(dto.getPassword()));
+        userRepository.save(user);
+        return ResponseDTO.success("Usuário registrado com sucesso");
+    }
+}
