b17853feed08d070e72ca748c28d453cbe7c2e14
Art <paganotiarthur@gmail.com>
2025-10-15 23:48:04 -0300

    :bug: fix(exception): improve handling for invalid credentials and forbidden operations

---
+++ src/main/java/com/spotify/exceptions/ForbiddenOperationException.java
@@ -0,0 +1,8 @@
+package com.spotify.exceptions;
+
+public class ForbiddenOperationException extends RuntimeException {
+    public ForbiddenOperationException(String message) {
+        super(message);
+    }
+}
+
--- src/main/java/com/spotify/exceptions/InvalidCredentialsException.java
+++ src/main/java/com/spotify/exceptions/InvalidCredentialsException.java
@@ -4,5 +4,8 @@ public class InvalidCredentialsException extends RuntimeException {
     public InvalidCredentialsException() {
         super("Usuário ou senha inválidos");
     }
-}
 
+    public InvalidCredentialsException(String message) {
+        super(message);
+    }
+}
