c365fcda06540b1170d3e226be5db6ffe3209a80
Art <paganotiarthur@gmail.com>
2025-10-15 22:08:04 -0300

    :sparkles: feat: add login endpoint and Swagger documentation

---
+++ src/main/java/com/spotify/services/AuthService.java
@@ -0,0 +1,33 @@
+package com.spotify.services;
+
+import com.spotify.business.dto.LoginRequestDTO;
+import com.spotify.business.dto.LoginResponseDTO;
+import com.spotify.business.security.JwtUtil;
+import com.spotify.entities.User;
+import com.spotify.exceptions.InvalidCredentialsException;
+import com.spotify.repositories.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+
+    public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtUtil jwtUtil) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtUtil = jwtUtil;
+    }
+
+    public LoginResponseDTO login(LoginRequestDTO loginRequest) {
+        User user = userRepository.findByEmail(loginRequest.getEmail())
+                .orElseThrow(InvalidCredentialsException::new);
+        if (!passwordEncoder.matches(loginRequest.getPassword(), user.getPassword())) {
+            throw new InvalidCredentialsException();
+        }
+        String token = jwtUtil.generateToken(user.getEmail());
+        return new LoginResponseDTO(token);
+    }
+}
