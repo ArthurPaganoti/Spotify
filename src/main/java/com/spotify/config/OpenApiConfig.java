e61fafd66863d6fc23e71794209d9ff623416daf
Art <paganotiarthur@gmail.com>
2025-10-15 23:18:56 -0300

    :sparkles: feat: add JWT Bearer authentication support to Swagger UI

--- src/main/java/com/spotify/config/OpenApiConfig.java
+++ src/main/java/com/spotify/config/OpenApiConfig.java
@@ -3,6 +3,8 @@ package com.spotify.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,7 +20,17 @@ public class OpenApiConfig {
                         .description("API Rest - Spotify")
                         .contact(new Contact()
                                 .name("Arthur Paganoti")
-                                .email("paganotiarthur@gmail.com")));
+                                .email("paganotiarthur@gmail.com")))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .name("Authorization")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                );
     }
 }
 
